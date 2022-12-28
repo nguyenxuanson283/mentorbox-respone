e21e0a368d807d9878782e96361a5e0744964abe
nguyenxuanson283 <nguyenxuanson283@gmail.com>
2022-12-28 14:44:47 +0700

    carousel item

--- main/main.js
+++ main/main.js
@@ -23,6 +23,30 @@ function clickOutSide(){
     const background = document.getElementById('backGroundMobile');
     background.classList.toggle('active')
 }
-function clickTest(){
-    console.log("ok")
+
+function nextPrev(){
+    var listImg = document.querySelector('.filtering')
+    var firstImg = listImg.querySelectorAll("img")[0]
+    var btnNext = document.getElementById('btnNext')
+    let firstImgWidth = firstImg.clientWidth + 10;
+    listImg.scrollLeft += firstImgWidth
+    if(listImg.scrollLeft > firstImgWidth * 4){
+        listImg.scrollLeft = 0;
+    }
+}
+function nextPrevTwo(){
+    var listImg = document.querySelector('.filtering-two')
+    var firstImg = listImg.querySelectorAll("img")[0]
+    var btnNext = document.getElementById('btnNext')
+    let firstImgWidth = firstImg.clientWidth;
+    listImg.scrollLeft += firstImgWidth + 20;
+    if(listImg.scrollLeft >= firstImgWidth * 4){
+        listImg.scrollLeft = 0;
+    }
+}
+
+
+function test(){
+    var listStar = document.querySelector('.start-carousel');
+    var firstStart = listStar.querySelectorAll("img")[0]
 }
